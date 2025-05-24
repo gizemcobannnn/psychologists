0261810ad7052faca28a793021496f47afde86de
gizemcoban <gizem.coban.r@gmail.com>
2025-05-24 20:38:59 +0200

    form style

--- psychologists/src/components/LoginModal/Login.jsx
+++ psychologists/src/components/LoginModal/Login.jsx
@@ -1,58 +1,60 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import { useId } from 'react';
-import * as Yup from 'yup';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useId } from "react";
+import * as Yup from "yup";
 
 export default function Login() {
-
   const emailId = useId();
   const passwordId = useId();
-  
+
   const loginValidate = Yup.object().shape({
     email: Yup.string().email().required("Required"),
-    password: Yup.string().min(9,"Password must be at least 9 characters.").max(18,"Password must be less than 18 characters.").required("Required"),
+    password: Yup.string()
+      .min(9, "Password must be at least 9 characters.")
+      .max(18, "Password must be less than 18 characters.")
+      .required("Required"),
   });
-  const handleLogin = () =>{
+  const handleLogin = () => {};
 
-  }
-  
   return (
-    <div className="authForm">
+    <div className="authForm flex flex-col items-start">
       <Formik
-        initialValues={{email:"",password:""}}
+        initialValues={{ email: "", password: "" }}
         validate={loginValidate}
         onSubmit={handleLogin}
-        >
-            <Form>
-                        <div className="fields">
-          <Field
-            name="email"
-            placeholder="Email*"
-            id={emailId}
-            type="email"
-            className="formField"
-          ></Field>
-          <ErrorMessage
-            name="email"
-            component="span"
-            className="errorMessages"
-          ></ErrorMessage>
-        </div>
+      >
+        <Form>
+          <div className="fields">
+          <h1 className="text-4xl">Log In</h1>
+          <p className="text-[#191A1580] opacity-50">Welcome back! Please enter your credentials to access your account and continue your search for a psychologist.</p>
+            <Field
+              name="email"
+              placeholder="Email"
+              id={emailId}
+              type="email"
+              className="formField"
+            ></Field>
+            <ErrorMessage
+              name="email"
+              component="span"
+              className="errorMessages"
+            ></ErrorMessage>
+          </div>
 
-        <div className="fields">
-          <Field
-            name="password"
-            placeholder="Password*"
-            id={passwordId}
-            type="password"
-            className="formField"
-          ></Field>
-          <ErrorMessage
-            name="password"
-            component="span"
-            className="errorMessages"
-          ></ErrorMessage>
-        </div>
-            </Form>
+          <div className="fields">
+            <Field
+              name="password"
+              placeholder="Password"
+              id={passwordId}
+              type="password"
+              className="formField"
+            ></Field>
+            <ErrorMessage
+              name="password"
+              component="span"
+              className="errorMessages"
+            ></ErrorMessage>
+          </div>
+        </Form>
       </Formik>
     </div>
   );
--- psychologists/src/components/RegisterModal/Register.jsx
+++ psychologists/src/components/RegisterModal/Register.jsx
@@ -1,68 +1,82 @@
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { useId } from "react";
-import * as Yup from 'yup';
+import * as Yup from "yup";
 
 export default function Register() {
-  
   const nameId = useId();
   const emailId = useId();
   const passId = useId();
   const registerSchema = Yup.object().shape({
-    name: Yup.string().min(3,"Name must be at least 3 characters.").max().required("Required field"),
-    email: Yup.string().email("Invalid email address.").required("Required field"),
-    password: Yup.string().min(9,"Password must be at least 9 characters.").max(18,"Password must be less than 18 characters.").required("Required field"),
+    name: Yup.string()
+      .min(3, "Name must be at least 3 characters.")
+      .max()
+      .required("Required field"),
+    email: Yup.string()
+      .email("Invalid email address.")
+      .required("Required field"),
+    password: Yup.string()
+      .min(9, "Password must be at least 9 characters.")
+      .max(18, "Password must be less than 18 characters.")
+      .required("Required field"),
   });
 
-  const handleSubmit = () =>{
-
-  }
+  const handleSubmit = () => {};
 
   return (
-    <div className="authForm">
-        <Formik
-            initialValues={{name:'',email:'',password:''}}
-            validation={registerSchema}
-            onSubmit={handleSubmit}>
-            <Form>
+    <div className="authForm flex flex-col items-start">
+      <Formik
+        initialValues={{ name: "", email: "", password: "" }}
+        validation={registerSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form>
+          <div className="fields">
+            <h1 className="text-4xl">Registration</h1>
+            <p className="text-[#191A1580] opacity-50">
+              Thank you for your interest in our platform! In order to register,
+              we need some information. Please provide us with the following
+              information.
+            </p>
+            <Field
+              name="name"
+              placeholder="Name"
+              type="text"
+              id={nameId}
+            ></Field>
+            <ErrorMessage
+              name="name"
+              component="span"
+              className="errorMessages"
+            ></ErrorMessage>
             <div>
-                <Field
-                    name="name"
-                    placeholder="Name*"
-                    type="text"
-                    id={nameId}></Field>
-                <ErrorMessage
-                    name="name"
-                    component="span"
-                    className="errorMessages">
-                </ErrorMessage>
+              <Field
+                name="email"
+                id={emailId}
+                placeholder="Email"
+                type="email"
+              ></Field>
+              <ErrorMessage
+                name="email"
+                component="span"
+                className="errorMessages"
+              ></ErrorMessage>
             </div>
             <div>
-                <Field
-                    name="email"
-                    id={emailId}
-                    placeholder="Email*"
-                    type="email"></Field>
-                <ErrorMessage
-                    name="email"
-                    component="span"
-                    className="errorMessages"
-                ></ErrorMessage>
+              <Field
+                name="password"
+                id={passId}
+                placeholder="Password"
+                type="password"
+              ></Field>
+              <ErrorMessage
+                name="password"
+                component="span"
+                className="errorMessages"
+              ></ErrorMessage>
             </div>
-            <div>
-                <Field
-                    name="password"
-                    id={passId}
-                    placeholder="Password"
-                    type="password"
-                    ></Field>
-                <ErrorMessage
-                    name="password"
-                    component="span"
-                    className="errorMessages">
-                </ErrorMessage>
-            </div>
-            </Form>
-        </Formik>
+          </div>
+        </Form>
+      </Formik>
     </div>
-  )
+  );
 }
