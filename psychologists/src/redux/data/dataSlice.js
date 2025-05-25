e2e8bf340e717717346e74b10307feda3dbf1b97
gizemcoban <gizem.coban.r@gmail.com>
2025-05-25 15:05:42 +0200

    slices

--- psychologists/src/redux/data/dataSlice.js
+++ psychologists/src/redux/data/dataSlice.js
@@ -1,22 +1,49 @@
 import { createSlice } from "@reduxjs/toolkit"
-
+import { fetchPsychologists } from "./dataOps"
 const initialState = {
     name: "psychologists",
     psychologists:[],
     error:null,
     filters:[],
     psychologist:{},
-    favorites:[]
+    favorites:[],
+    loading:false
 }
 
 export const psychologistSlice = createSlice({
     initialState,
     name:initialState.name,
-    reducers:{},
+    reducers:{
+        setFilters:(state,action)=>{
+            state.filters = {...state.filters, ...action.payload}
+        },
+        setFavorites: (state,action)=>{
+            const id = action.payload;
+            if(state.favorites.includes(id)){
+                state.favorites=state.favorites.filter(idx=>idx!==id)
+            }else{
+                state.favorites.push(id);
+            }
+
+        }
+    },
     extraReducers: (builder)=> {builder
-        .addCase()
-        .addCase()
-        .addCase()
+        .addCase(fetchPsychologists.fulfilled,(state,action)=>{
+            state.psychologist = action.payload;
+            state.error = null;
+             state.loading = false;
+
+        })
+        .addCase(fetchPsychologists.pending,(state)=>{
+            state.psychologist = [];
+            state.error = null;
+            state.loading = true;
+        })
+        .addCase(fetchPsychologists.failed,(state,action)=>{
+            state.psychologist = [];
+            state.error = action.payload;
+            state.loading = false;
+        })
     }
 })
 
