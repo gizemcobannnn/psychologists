659bb30eae3a211adb63b7cf84a2e0bc41cc466c
gizemcoban <gizem.coban.r@gmail.com>
2025-05-25 18:33:00 +0200

    psyc page initiliaze

--- psychologists/src/App.jsx
+++ psychologists/src/App.jsx
@@ -4,14 +4,15 @@ import Register from './components/RegisterModal/Register'
 import Login from './components/LoginModal/Login'
 import HomePage from './pages/HomePage'
 import ReadFirebase from './components/ReadFirebase/ReadFirebase'
-
+import PsychologistPage from './pages/PsychologistPage'
 
 function App() {
 
   return (
     <>
       {/**<AppRoute/>   <HomePage/>    <Register></Register>    <Login></Login> */}
-      <ReadFirebase></ReadFirebase>
+      {/** <ReadFirebase></ReadFirebase> */}
+      <PsychologistPage></PsychologistPage>
 
     </>
   )
